6a33174284ef2402a489407b1a0e7ea1b1a8a6b7
German Alvarez <ger.22.alvarez@gmail.com>
2020-09-02 16:09:04 -0300

    backend

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,18 @@
-function capturar() {
+function sendMessage() {
     // Obtenemos el valor por el id
-    var porId = document.getElementById("telefono").value;
-    window.open(`https://api.whatsapp.com/send?phone=+549${porId}`, '_blank')
+    let codArea = document.getElementById("cod-area").value; 
+    var number = document.getElementById("cod-numero").value;
+    var msgWithSpaces = document.getElementById("messageInput").value;
+
+
+    var msgWithoutSpaces = msgWithSpaces.split(' ').join('%20');
+    
+    if(codArea == '' || number == ''){
+      $("#test").removeClass('d-none');
+      
+      setTimeout(function(){ 
+        $("#test").addClass('d-none'); 
+      }, 4000)
+    };
+    window.open(`https://api.whatsapp.com/send?phone=${codArea}${number}&text=${msgWithoutSpaces}`, '_blank')
   }
